e3a14b45e03f90cba591288cc24767e68d406d1b
andrew000ppp <47956693+andrew000ppp@users.noreply.github.com>
2019-03-24 17:08:30 -0700

    final

---
+++ index.js
@@ -0,0 +1,47 @@
+/* var a = 10%8*3/2
+
+//alert(a)
+
+var arrayA = [1, 2, 5, 6, 10]
+
+alert(arrayA)
+
+arrayA.push(" NEW ELEMENT!!!!!!")
+
+alert(arrayA)
+
+alert(arrayA.slice(2))
+
+arrayA.push(" HELLO!")
+
+alert(arrayA)
+
+alert(arrayA.sort())
+
+var boo = false && true || !(true && true)
+
+alert(boo)
+
+if (a==3) {
+	alert(true)
+} else {
+	alert(false)
+}
+
+function addSquares(x, y) {
+	return(x*x + y*y)
+}
+
+alert(addSquares(2, 3))
+
+function concat(str1, str2) {
+	return(str1 + str2)
+}
+
+var newstring = concat("Hello ", "World")
+
+alert(newstring)
+
+function triProduct(x, y, z) {
+	alert(x*y*z)
+}
